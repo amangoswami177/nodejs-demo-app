b487a46062eeb93d360d61d40bdb2f8fb2832691
aman <ramgoswami3@gmail.com>
2025-04-07 15:24:34 +0530

    done now maybe

--- test/test/app.test.js
+++
@@ -1,8 +0,0 @@
-// test/app.test.js
-const assert = require('chai').assert;
-
-describe('Sample Test', function() {
-  it('should return true', function() {
-    assert.equal(true, true);
-  });
-});
